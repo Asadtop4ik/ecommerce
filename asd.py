82cec9de8fb3f36559fc317a1589ba883b6d1d8f
asadtop4ik <asadbek.backend@gmail.com>
2024-04-03 14:48:00 +0500

    new project

--- asd.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.utils import get_random_secret_key
-
-# Generate a new secret key
-new_secret_key = get_random_secret_key()
-
-# Print the new secret key
-print(new_secret_key)
